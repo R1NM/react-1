1cf5e4c2d2790fd23a3af57635850ea4062c0c4d
rinm.desktop <shinzeya@gmail.com>
2022-06-21 20:40:30 +0900

    state and props

---
+++ src/components/TOC.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+
+class TOC extends Component{
+    render(){
+        var data = this.props.data;
+        var lists=[];
+        var i =0;
+        while(i<data.length){
+            lists.push(
+            <li key={data[i].id}>
+                <a 
+                    href={"/content/"+data[i].id}
+                    data-id={data[i].id}
+                    onClick={(e)=>{
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }}>
+                    {data[i].title}
+                </a>
+            </li>)
+            i=i+1;
+        }
+    return(
+        <nav>
+        <ul>
+            {lists}
+        </ul>
+        </nav>
+    )
+    }
+}
+
+export default TOC;
